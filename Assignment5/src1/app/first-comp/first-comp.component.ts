54332eef291998a26f196c312acb20779c1cacea
an-rutikshirke <rutik.s@applicationnexus.com>
2023-12-25 21:06:23 +0530

    added assignmet 5solution

---
+++ Assignment5/src1/app/first-comp/first-comp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './first-comp.component.html',
+  styleUrl: './first-comp.component.css',
+})
+export class FirstCompComponent {}
